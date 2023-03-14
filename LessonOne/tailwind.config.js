7a28796303f63581a57e95850081a16de8366250
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-03-15 00:02:45 +0530

    customize color in tailwind.config.json file

--- LessonOne/tailwind.config.js
+++ LessonOne/tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./output/**/*.html"],
   darkMode: 'media',
   theme: {
-    extend: {},
+    
+    extend: {
+      colors:{
+        MyColor:{
+          300: "#D9D5D5"
+        },
+        pink: {
+          light: '#ff7ce5',
+          DEFAULT: '#ff49db',
+          dark: '#ff16d1',
+        },
+      },
+    },
   },
   plugins: [],
 }
